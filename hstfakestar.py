1e3e69463a0204bd3853694780a19b1e2cafa06d
srodney <steve.rodney@gmail.com>
2015-10-01 13:37:18 -0400

    added the addtofits() function... not really complete?

---
+++ hstfakestar.py
@@ -0,0 +1,42 @@
+__author__ = 'rodney'
+
+from hstphot import radec2xy
+import pyfits
+import numpy as np
+from PythonPhot.photfunctions import rdpsfmodel
+from PythonPhot.fakestar import addtoimarray
+
+def addtofits(fitsin, fitsout, psfmodelfile, position, fluxscale,
+              coordsys='xy', verbose=False):
+    """ Create a psf, add it into the given fits image at the
+
+    :param fitsin: fits file name for the target image
+    :param psfmodelfile: fits file holding the psf model
+    :param position: Scalar or array, giving the psf center positions
+    :param fluxscale: Scalar or array, giving the psf flux scaling factors
+    :param coordsys: Either 'radec' or 'xy'
+    :return:
+    """
+
+    if not np.iterable(fluxscale):
+        fluxscale = np.array([fluxscale])
+        position = np.array([position])
+
+    if coordsys == 'radec':
+        ra = position[:, 0]
+        dec = position[:, 1]
+        position = radec2xy(fitsin, ra, dec, ext=0)
+
+    image = pyfits.open(fitsin, mode='readonly')
+    imagedat = image[0].data
+
+    psfmodel = rdpsfmodel(psfmodelfile)
+
+    for i in range(len(fluxscale)):
+        imagedat = addtoimarray(imagedat, psfmodel, position[i], fluxscale[i])
+    image[0].data = imagedat
+
+    image.writeto(fitsout, clobber=True)
+    if verbose:
+        print("Wrote updated image to %s" % fitsout)
+    return
